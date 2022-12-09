a6295f0a88e1f2fc04efe3e6592fc29618bb680a
Your Name <you@example.com>
2022-12-09 15:01:34 -0800

    day two

--- Creperia.java
+++ Creperia.java
@@ -10,8 +10,8 @@ public class Creperia extends JPanel{
 
 private Image[] allPics;
 private ArrayList<Image> pics;
-private int myWindowWidth = 300;
-private int myWindowHeight = 400;
+private int myWindowWidth = 700;
+private int myWindowHeight = 600;
 
 public Creperia() {
     JFrame easel = new JFrame();		
@@ -33,12 +33,33 @@ public void paintComponent (Graphics g) {
 
 
     public int chooseSize(){
+        //int x = JOptionPane.showOptionDialog(null, message: "Choose a size.", 
+            //title: "Prep Station",
+            //JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, sizes, sizes[0]);
+            
         return 0;
     }
     
 
     public String chooseFilling(){
+String[] fillings = {"abc", "def", "ghi", "jkl"};
 
-        return null;
+        int x = JOptionPane.showOptionDialog(null, "Choose a filling.",
+                "Filling Station",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, fillings, fillings[0]);
+        // System.out.println(x);
+        if(x==0){
+            return "you chose nutella";
+        }else if(x==1){
+            return "you chose sugar and lemon";
+        }else if(x==2){
+            return "you chose strawberry jam";
+        }else if(x==3){
+            return "you chose salted caramel";
+        }else if(x==4){
+            return "you chose cinnamon sugar";
+        }else {
+            return null;
+        }
     }
 }
