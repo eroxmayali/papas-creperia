a6eada69bc9aa35830fc019e0225aeb32d483529
Your Name <you@example.com>
2023-01-04 15:06:26 -0800

    Co-authored-by: eroxmayali <eroxmayali@users.noreply.github.com>

--- Creperia.java
+++ Creperia.java
@@ -3,15 +3,17 @@ import javax.swing.*;
 import java.awt.image.*;
 import javax.imageio.*;
 import java.io.File;
-import java.util.Random;
+import java.util.*;
 import java.util.ArrayList;
+
 
 public class Creperia extends JPanel{
 
-private Image[] allPics;
-private ArrayList<Image> pics;
 private int myWindowWidth = 700;
 private int myWindowHeight = 600;
+private String ImageToShow = "logo.png";
+private int index = 0;
+private int mySize = 0;
 
 public Creperia() {
     JFrame easel = new JFrame();		
@@ -24,7 +26,7 @@ public Creperia() {
 public void paintComponent (Graphics g) {
     System.out.println("asdfd");
     try{
-      BufferedImage myPic = ImageIO.read(new File("logo.png"));
+      BufferedImage myPic = ImageIO.read(new File(ImageToShow));
       g.drawImage(myPic, 0, 0, null);	
    } catch(Exception e){
         e.printStackTrace();
@@ -39,17 +41,20 @@ public void paintComponent (Graphics g) {
                 "Batter Station",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, sizes, sizes[0]);
         if(x == 0){
+            this.cook();
             return "you chose small";
         }else if(x == 1){
+            this.cook();
             return "you chose medium";
         }else if(x == 2){
+            this.cook();
             return "you chose large";
         }else{
             return null;
         }
     }
     
-    
+
 
     public String chooseFilling(){
 String[] fillings = {"Nutella", "Sugar and Lemon", "Strawberry Jam", "Salted Caramel", "Cinnamon Sugar"};
@@ -59,14 +64,24 @@ String[] fillings = {"Nutella", "Sugar and Lemon", "Strawberry Jam", "Salted Car
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, fillings, fillings[0]);
         // System.out.println(x);
         if(x==0){
+            ImageToShow = "nutella.png";
+            this.repaint();
             return "you chose nutella";
         }else if(x==1){
+            ImageToShow = "lemon.png";
+            this.repaint();
             return "you chose sugar and lemon";
         }else if(x==2){
+            ImageToShow = "strawb.png";
+            this.repaint();
             return "you chose strawberry jam";
         }else if(x==3){
+            ImageToShow = "caramel.png";
+            this.repaint();
             return "you chose salted caramel";
         }else if(x==4){
+            ImageToShow = "cinsug.png";
+            this.repaint();
             return "you chose cinnamon sugar";
         }else {
             return null;
@@ -89,4 +104,41 @@ String[] toppings = {"Whipped Cream", "Banana", "Strawberry"};
             return null;
         }
     }
+
+public int calcScore(){
+    return 0;
 }
+
+ArrayList<String> pics = new ArrayList<String>(){
+    {
+    add("crepemaking1.png");
+    add("crepemaking2.png");
+    add("crepemaking3.png");
+    add("crepemaking4.png");
+    add("crepemaking5.png");
+    add("crepemaking6.png");
+    add("crepemaking7.png");
+    add("crepemaking8.png");
+    add("crepemaking9.png");
+    add("crepemaking10.png");
+    }
+};
+
+public void cook(){
+    java.util.Timer timer = new java.util.Timer();
+    timer.schedule(new TimerTask() {
+ 	    public void run() {
+            ImageToShow = pics.get(index);
+            index++;
+			repaint();
+            if(index==10){
+                timer.cancel();
+            }
+    }
+ }, 0, 1000);
+}
+
+
+
+}
+
--- CreperiaMain.java
+++ CreperiaMain.java
@@ -2,6 +2,9 @@ public class CreperiaMain{
     public static void main(String[] args) {
             //new Customer();
            Creperia c1 = new Creperia();
-          c1.chooseSize();
+    c1.chooseSize();
+    c1.chooseFilling();
+    c1.chooseTopping();
+    c1.calcScore();
     }
 }
